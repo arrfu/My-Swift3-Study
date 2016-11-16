8b6bd021e498320c7aea184c1087aca07d6ccfbe
lujianfu <lujianfu@snaillove.com>
2016-11-16 17:10:12 +0800

    no message

--- swift3StudyDemo/JFClosuresDemo/JFClosuresDemo/ViewController.swift
+++ swift3StudyDemo/JFClosuresDemo/JFClosuresDemo/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by hao123 on 2016/11/16.
 //  Copyright © 2016年 arrfu. All rights reserved.
-//
+//  闭包有点类似oc的block代码块
 
 /*
  闭包的语法表达式：
@@ -31,6 +31,15 @@ class ViewController: UIViewController {
         
         // 闭包形式3 （当闭包没有接收参数时，可省略in关键字）
         testClosure3()
+        
+        // 闭包形式4.简化（省略参数名，用$0,$1,$2,$3,...代替）
+        testClosure4()
+        
+        // 闭包的使用：值捕获
+        testClosure5()
+        
+        // 闭包的使用：尾随闭包
+        testClosure6()
     }
 
     override func didReceiveMemoryWarning() {
@@ -81,4 +90,66 @@ func testClosure3(){
     
     let reslut = closureAdd()
     print("reslut = \(reslut)")
+}
+
+// 闭包形式4
+func testClosure4(){
+    // 4.简化（省略参数名，用$0,$1,$2,$3,...代替）
+    let closureAdd:(Int,Int) -> Int
+    
+    closureAdd = {
+        return $0+$1
+    }
+    
+    let reslut = closureAdd(2,3)
+    print("reslut = \(reslut)")
+}
+
+// 闭包的使用：值捕获
+func testClosure5(){
+    // 值捕获
+    func makeIncrease(forIncrese num:Int) ->() -> Int{
+        var total = 0
+        
+        // 嵌套函数，捕获上下文变量
+        func increastor() -> Int{
+            total += num
+            return total
+        }
+        return increastor // 返回函数类型为() -> Int
+    }
+    
+    let increaseFunc1 = makeIncrease(forIncrese: 3)
+    print(increaseFunc1())// 3
+    print(increaseFunc1())// 6
+    print(increaseFunc1())// 9
+    
+    let increaseFunc1_1 = increaseFunc1 // 引用同一个闭包
+    print(increaseFunc1_1())// 12
+    
+    let increaseFunc2 = makeIncrease(forIncrese: 5) // 新建一个新的闭包
+    print(increaseFunc2())// 5
+    print(increaseFunc2())// 10
+    print(increaseFunc2())// 15
+    
+}
+
+// 闭包的使用：尾随闭包
+func testClosure6(){
+    // 参数：一个Int型参数，一个函数型参数,返回值：String型
+    func outputNameAndNumber(_ num:Int,name:(String) -> String) -> String{
+        let infoString:String
+        infoString = name("Peater")+"My num is \(num)"
+        return infoString
+    }
+    
+    // 不使用尾随闭包写法：
+//    let result = outputNameAndNumber(24, name: {
+//        (String) in
+//        let infoString = "My name is \($0)"
+//        return "My name is \($0)"})
+//    
+//    print(result)
+    
+    
 }
